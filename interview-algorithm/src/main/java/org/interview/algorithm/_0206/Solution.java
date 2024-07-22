828ac345d638bece70c005392d10e0a6f55cee17
Jiajun Chen <chenjiajun@hnu.edu.cn>
2024-07-22 23:27:49 +0800

    feat: 首次提交

---
+++ interview-algorithm/src/main/java/org/interview/algorithm/_0206/Solution.java
@@ -0,0 +1,68 @@
+package org.interview.algorithm._0206;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        return iteration(head);
+    }
+
+    // 递归
+    public ListNode recursion(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        ListNode newHead = reverseList(next);
+        next.next = head;
+        return newHead;
+    }
+
+    // 迭代
+    public ListNode iteration(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        while (next != null) {
+            ListNode next2 = next.next;
+            next.next = head;
+            head = next;
+            next = next2;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        Solution solution = new Solution();
+        solution.reverseList(n1);
+    }
+}
